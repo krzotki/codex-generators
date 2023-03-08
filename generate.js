419d36cccf3b4e87d6673d6fe530f4be2768aac2
Karol Rzotki <krzoti@gmail.com>
2023-03-08 23:29:04 +0100

    improve

--- generate.js
+++ generate.js
@@ -12,28 +12,63 @@ const context = `
 # The following is a React.js component generator. 
 # User provides 'name' and 'props' and in return he gets a component named with provided 'name' that uses provided 'props'.
 # User also provides "classNames" that are used for styling elements. The project is using css modules.
-# This is example output when user provided name="Button", props="[label,onClick]", classNames=["button", "label"]:
+# User also provides "dataTestId" that is passed to main component and is used to find given element in tests.
+# User also provides "events" which are names of events that will be dispatched in component.
+# User also provides "copy" which is record of strings. Those string are used to display text in component.
+# This is example output when user provided name="Button", props=["imageSrc","type"], classNames=["button", "image", "label"], data-testid="button-test-id", events=["OnClick"], copy=["altText", "label"]:
 
 `;
 
 const example = `
-  // Button.tsx
+  // FILE: Button.tsx
 
   import css from './Button.module.css'
+  import {dispatch} from '@brainly/brainiac'
+  import {ButtonEventsType} from './ButtonEventsType';
 
   type PropsType = {
-    label: string
-    onClick: () => void
+    imageSrc: string;
+    someId: number;
+    copy: ReadonlyArray<{
+      altText: string;
+      label: string;
+    }>
   }
   
-  export const Button = ({label, onClick}: PropsType) => { 
-    return <button className={css.button} onClick={onClick}>
-    <span className={css.label}>{label}</span>
-    </button> 
+  const Button = ({label, someId, imageSrc, copy}: PropsType) => { 
+    const handleClick = React.useCallback((e) => {
+      dispatch<ButtonOnClickType>(e.target, [
+        ButtonEventsType.ON_CLICK,
+        {
+          someValue: someId
+        }
+      ])
+    }, [someId]);
+
+    return (
+      <button className={css.button} onClick={handleClick} data-testid="button-test-id">
+        <img alt={copy.altText} src={imageSrc}/>
+        <span className={css.label}>{copy.label}</span>
+      </button> 
+    )
   }
 
+  export default React.memo<PropsType>(Button);
 
-  // Button.module.css
+  // FILE: ButtonEventsType.ts
+
+  export enum ButtonEventsType {
+    ON_CLICK = 'BrnButtonOnClick'
+  }
+
+  export type ButtonOnClickType = [
+    ButtonEventsType.ON_CLICK,
+    {
+      someValue: number
+    }
+  ]
+
+  // FILE: Button.module.css
 
   .button {
 
@@ -43,41 +78,87 @@ const example = `
 
   }
 
-  // Button.spec.tsx
-  import {render, screen} from '@testing-library/react'
-  import '@testing-library/jest-dom'
+  .image {
 
-  const defaultProps = {
-    label: 'test label value'
-    onClick: () => undefined
   }
 
-  it('renders Button', async () => {
-    render(<Button {...defaultProps} />)
+  // FILE: Button.spec.tsx
+  import {render, screen} from '@testing-library/react'
+  import '@testing-library/jest-dom'
+  import {ButtonEventsType} from './ButtonEventsType';
+  import {promiseFromEvent} from '@brainly/brainiac'
 
-    expect(screen.getByText('test label value')).toBeInTheDocument();
+  const defaultProps = {
+    imageSrc: "test imageSrc",
+    someId: 1,
+    copy: {
+      altText: "test altText",
+      label: "test label",
+    }
+  }
+  
+
+  describe("<Button />", () => {
+    it('renders Button', async () => {
+      render(<Button {...defaultProps} />)
+  
+      expect(screen.getByTestId('button-test-id')).toBeInTheDocument();
+    })
+
+    it('fires OnClick event', async () => {
+      render(<Button {...defaultProps} />)
+  
+      const eventPromise = promiseFromEvent(ButtonEventsType.ON_CLICK);
+      const {detail: eventDetail} = await eventPromise();
+
+      expect(eventDetail).toEqual({someValue: defaultProps.someId})
+    })
   })
 
-  // Button.stories.tsx
+  // FILE: Button.stories.tsx
   import {storiesOf} from '@storybook/react'
-  import {OrderedList} from './OrderedList'
+  import {Button} from './Button'
+  import {text, number, object} from '@storybook/addon-knobs'
 
-  storiesOf('OrderedList', module)
-    .add('default', () => <OrderedList items={['item1', 'item2', 'item3']} onItemClick={() => undefined} />)
-    .add('empty', () => <OrderedList items={[]} onItemClick={() => undefined} />)
+  storiesOf('Button', module)
+    .add('default', () => (
+      <Button 
+        imageSrc={text('imageSrc', 'test imageSrc')} 
+        someId={number('someId', 1)}
+        copy={
+          object('copy', {      
+            altText: "test altText",
+            label: "test label"
+          })
+        }
+      />
+    ))
+
+  // FILE: index.ts
+  export {default as Button} from './Button'
+  export * from './ButtonEventsType';
+
+  // END_MARK
   `;
 
 const options = {
   name: "OrderedList",
-  props: ["items", "onItemClick"],
-  classNames: ["list", "listItem"],
+  props: ["items", "type", "emoji"],
+  classNames: ["list", "listItem", "heading", "description"],
+  dataTestId: "my-ordered-list",
+  events: ["OnItemClick", "OnListClick"],
+  copy: ["headingText", "descriptionText"]
 };
 
 const request = `
-# Create a component, sample css module file, sample jest test file and sample storybook file for the following options:  
+# For the following options:  
 # name="${options.name}"
-# props="${JSON.stringify(options.props)}"
-# classNames="${JSON.stringify(options.classNames)}"
+# props=${JSON.stringify(options.props)}
+# classNames=${JSON.stringify(options.classNames)}
+# dataTestId="${options.dataTestId}"
+# events=${JSON.stringify(options.events)}
+# Create a component, events file, sample css module file, sample jest test file, sample storybook file:
+
 `;
 
 const generate = async () => {
@@ -89,16 +170,16 @@ const generate = async () => {
     ${request}
     `,
     temperature: 0,
-    max_tokens: 1024,
+    max_tokens: 1500,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
-    stop: ["#"],
+    stop: ["END_MARK", "#", "// END_MARK"],
   });
 
   const result = response.data.choices[0].text;
 
-  console.log(result);
+  console.log(result, response.data);
   fs.writeFile(`${options.name}.tsx`, result, { flag: "w+" }, (err) => {
     if (err) {
       console.error(err);
